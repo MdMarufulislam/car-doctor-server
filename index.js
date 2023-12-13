d4d21c3395fdb2be51635772bb58d112a545fd96
Md Maruf Ul Islam <marufulislam911@gmail.com>
2023-12-14 02:13:48 +0600

    server 1

--- index.js
+++ index.js
@@ -7,10 +7,7 @@ require('dotenv').config()
 const app=express();
 const port= process.env.PORT || 5000
 
-app.use(cors({
-  origin:["http://localhost:5173"],
-  credentials: true
-}));
+app.use(cors());
 app.use(express.json())
 app.use(cookieParser())
 
