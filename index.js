e1d1ed3e0c2a229da03415df34ae071522d2be10
Md Maruf Ul Islam <marufulislam911@gmail.com>
2023-12-14 02:50:09 +0600

    cors

--- index.js
+++ index.js
@@ -7,7 +7,10 @@ require('dotenv').config()
 const app=express();
 const port= process.env.PORT || 5000
 
-app.use(cors());
+app.use(cors({
+  origin:["https://car-doctor-five.vercel.app"],
+  credentials: true
+}));
 app.use(express.json())
 app.use(cookieParser())
 
